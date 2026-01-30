6a5067dffe0ca27e665cc7759da02ddeae3f1a92
habiba-shoukry <hs2115@hw.ac.uk>
2026-01-30 15:59:01 +0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,19 +14,24 @@ import GoalsProgress from "./pages/GoalsProgress";
 import ActivityFoodLog from "./pages/ActivityFoodLog";
 import Notifications from "./pages/Notifications";
 import Chatbot from "./pages/Chatbot";
-import Welcome from "./pages/Welcome"; 
+import Welcome from "./pages/Welcome";
+import LogIn from './pages/LogIn'; 
+import SignUp from './pages/SignUp'; 
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         {/* ----------------------------------------------- */}
-        {/* GROUP 1: PUBLIC PAGES (No Sidebar, No Layout)   */}
+        {/* GROUP 1:  No Sidebar, No Layout                 */}
         {/* ----------------------------------------------- */}
         <Route path="/" element={<Welcome />} />
+        <Route path="/LogIn" element={<LogIn />} />   
+        <Route path="/SignUp" element={<SignUp />} />
 
         {/* ----------------------------------------------- */}
-        {/* GROUP 2: APP PAGES (With Sidebar & Chatbox)     */}
+        {/* GROUP 2: app pages (With Sidebar & Chatbox)     */}
         {/* use "/*" to catch all other links and apply  */}
         {/* the Layout to them.                             */}
         {/* ----------------------------------------------- */}
